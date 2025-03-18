4d3eeefe77ee804b7862893f33c868fa1886cb71
DhanaCoder <dhanaraja@rekonsys.tech>
2025-03-18 21:52:06 +0530

    meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Search Console Verification */}
+        <meta
+          name="google-site-verification"
+          content="sMi7fG2aND_shfyyl7ykZVUnJtcKSWwcp3EQZajIPTQ"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
